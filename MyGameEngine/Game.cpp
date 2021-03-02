950c738c6ee4f349a1256e862be312c38e5b2d68
SamuelPavlik <pavlik.samuel@gmail.com>
2021-03-02 14:29:37 +0100

    Created Game and Window classes

---
+++ MyGameEngine/Game.cpp
@@ -0,0 +1,13 @@
+#include "Game.hpp"
+
+void Game::Update() {}
+
+void Game::LateUpdate() {}
+
+void Game::Draw() {}
+
+bool Game::IsRunning() const {
+	// We’ll return true here for now but this will be 
+	// changed shortly as we need a method of closing the window.
+	return true;
+}
--- MyGameEngine/main.cpp
+++ MyGameEngine/main.cpp
@@ -1,21 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    Game game;
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+    while (game.IsRunning()) {
+        game.Update();
+        game.LateUpdate();
+        game.Draw();
     }
-
-    return 0;
 }
