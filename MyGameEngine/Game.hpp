e74a2d92e5b032883c4a32e695c9d3e40c71bf0f
SamuelPavlik <pavlik.samuel@gmail.com>
2021-03-03 09:43:23 +0100

    L1 done; updated lib in linker input to ...-d.lib

--- MyGameEngine/Game.hpp
+++ MyGameEngine/Game.hpp
@@ -1,13 +1,20 @@
 #ifndef Game_hpp
 #define Game_hpp
 
+#include "Window.hpp"
+
 class Game
 {
 public:
+    Game() : window("Game Window") {}
     void Update();
     void LateUpdate();
     void Draw();
     bool IsRunning() const;
+
+private:
+    Window window;
 };
+
 
 #endif /* Game_hpp */
